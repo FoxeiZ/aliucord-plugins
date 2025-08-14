e3a0e4b7635ad08838d8f556f58d0762669c3cc4
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-08-14 22:56:52 +0700

    bump version

--- plugin/shareX/build.gradle.kts
+++ plugin/shareX/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.2.0" // Plugin version. Increment this to trigger the updater
+version = "1.3.0" // Plugin version. Increment this to trigger the updater
 description =
     "Upload >10mb file for non-nitro user." // Plugin description that will be shown to user
 
@@ -6,16 +6,7 @@ aliucord {
     // Changelog of your plugin
     changelog.set(
         """
-        feat: Add Litterbox upload support and refactor command system
-
-        - Add Litterbox as new upload provider with configurable time limits (1h, 12h, 24h, 72h)
-        - Implement dynamic command registration for upload providers
-        - Refactor settings to use factory pattern for extra settings and commands
-        - Extract utility functions to PluginUtils for text input creation
-        - Rename CatboxSetting to CatboxSettings for consistency
-        - Update UploadProcessor to use factory pattern for provider creation
-        - Add CatboxCommands for provider-specific commands
-        - Replace unused exception variable with underscore
+        feat: add Pomf provider with custom url
         """.trimIndent()
     )
     // Image or Gif that will be shown at the top of your changelog page
@@ -27,7 +18,7 @@ aliucord {
 
     // Excludes this plugin from the updater, meaning it won't show up for users.
     // Set this if the plugin is unfinished
-    excludeFromUpdaterJson.set(true)
+    excludeFromUpdaterJson.set(false)
 }
 
 android {
