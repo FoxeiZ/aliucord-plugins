01b45c9c0795358ff1c5a93457e4766f55752f41
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2026-03-10 22:35:09 +0700

    chore(SelectableText): bump version

--- plugins/SelectableText/build.gradle.kts
+++ plugins/SelectableText/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "0.01"
+version = "0.1.2"
 description =
     "Make every text selectable!"
 
@@ -9,7 +9,7 @@ aliucord {
         """.trimIndent()
     )
 
-    deploy.set(false)
+    deploy.set(true)
 }
 
 // android {
